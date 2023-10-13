bde2d4ac692a1c6b508671ee18021c93c7e85166
Winycios <winycios.nascimento@sptech.school>
2023-10-13 04:37:36 -0300

    Atualização na tabela de ocorrencias

--- tables_supervisiON.sql
+++ tables_supervisiON.sql
@@ -1,3 +1,4 @@
+	-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 CREATE DATABASE SuperVisiON;
 
 USE SuperVisiON;
@@ -7,7 +8,9 @@ CREATE TABLE tbOrganizacao(
 cnpj CHAR(14) PRIMARY KEY,
 nome VARCHAR(100)
 );
-	
+
+select * from tbComputador;
+SELECT idComputador FROM tbComputador WHERE apelidoComputador = 'PC1 loud' AND fk_idEvento = (SELECT idEvento FROM tbEvento WHERE status = 'Em andamento');
 CREATE TABLE tbUsuario(
 
 cpf CHAR(11) PRIMARY KEY ,
@@ -19,7 +22,7 @@ cargo VARCHAR(50),
 statusServico VARCHAR(45),
 FOREIGN KEY (fk_organizacao) REFERENCES tbOrganizacao(cnpj)
 );
-	
+
 CREATE TABLE tbEvento(
 
 idEvento INT PRIMARY KEY AUTO_INCREMENT,
@@ -30,13 +33,12 @@ time2 VARCHAR(50),
 status VARCHAR(20),
 FOREIGN KEY (fk_organizacao) REFERENCES tbOrganizacao(cnpj)
 );
-    
+                
 CREATE TABLE tbComputador(
 
 idComputador INT PRIMARY KEY AUTO_INCREMENT,
 fk_idEvento INT,
 apelidoComputador VARCHAR(50),
-numCpu INT,
 capMaximaCpu INT,
 capMaximaGpu INT,
 capMaximaDisco INT,
@@ -47,12 +49,16 @@ FOREIGN KEY (fk_idEvento) REFERENCES tbEvento(idEvento)
 CREATE TABLE tbOcorrencia(
 
 idOcorrencia INT PRIMARY KEY AUTO_INCREMENT,
+descricao VARCHAR(100),
 fk_idComputador INT,
+hora datetime,
 fk_cpfOperador CHAR(11),
 status VARCHAR(45),
 FOREIGN KEY (fk_cpfOperador) REFERENCES tbUsuario(cpf),
 FOREIGN KEY (fk_idComputador) REFERENCES tbComputador(idComputador)
 );
+
+
 
 CREATE TABLE tbMonitoramento(
 
@@ -68,6 +74,19 @@ disco INT,
 ram INT,
 FOREIGN KEY (fk_idComputador) REFERENCES tbComputador(idComputador)
 );
+
+
+/* dados do monitoramento*/
+INSERT INTO tbMonitoramento (dataHora, cpuTemp, cpuFreq, gpuTemp, gpuFreq, redeLatencia, redePacote)
+VALUES ('2023-09-16 11:05:00', 30, 20, 25, 10, 20, 1);
+
+INSERT INTO tbMonitoramento (dataHora, cpuTemp, cpuFreq, gpuTemp, gpuFreq, redeLatencia, redePacote)
+VALUES
+    ('2023-09-16 11:10:00', 64, 68, 50,  69, 40, 1);
+    
+INSERT INTO tbMonitoramento (dataHora, cpuTemp, cpuFreq, gpuTemp, gpuFreq, redeLatencia, redePacote)
+VALUES
+   ('2023-09-16 11:15:00', 80, 90, 100,  10, 200, 1);
 
 create user 'superOn'@'localhost' identified by '123';
 grant all privileges on SuperVisiON.* to 'superOn'@'localhost';
